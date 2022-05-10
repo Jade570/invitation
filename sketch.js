b52d914f17e5b95b057fa46651179974f5430e43
jade570 <chaelyoung@naver.com>
2022-05-10 23:50:33 +0900

    Update sound

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 let people = [];
 let peopleNum = 2;
 let tg;
+let start = false;
 let mp = false;
 let change = false;
 let i = 1;
 let rain=[];
 let raining= false;
+let sound1, sound2, sound3;
+let playS3 = false;
 
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * max);
@@ -44,6 +47,13 @@ class Person {
 };
 
 
+function preload(){
+  soundFormats('wav');
+  sound1 = loadSound('assets/1.wav');
+  sound2 = loadSound('assets/2.wav');
+  sound3 = loadSound('assets/33.wav');
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
@@ -77,67 +87,81 @@ function setup() {
 }
 
 function draw() {
-  for (let i = 0; i < people.length; i++) {
-    if (people[i].startDeg<people[i].endDeg){ //시계방향
-      people[i].i ++;
-    }
-    else{ //반시계방향
-      people[i].i --;
-    }
-    if (people[i].i === people[i].endDeg){
-      people[i].direction = !people[i].direction;
-      people[i].radius = 30+getRandomInt(150)
-      people[i].startDeg = (people[i].endDeg+180)%360;
-      people[i].i = people[i].startDeg;
-      people[i].endDeg =  people[i].direction ? people[i].startDeg+45+getRandomInt(270) :people[i].startDeg-45-getRandomInt(270); 
-      people[i].xAxis = people[i].x-people[i].radius*cos(radians(people[i].i))
-      people[i].yAxis = people[i].y-people[i].radius*sin(radians(people[i].i))
-      
-    }
-    people[i].x=this.xAxis-cos(radians(this.i))*this.radius;
-    people[i].y=this.yAxis-sin(radians(this.i))*this.radius; 
-    people[i].render();
-  }
-  if (mp){
-    if(i<255 && !change){
-      background(i*1.1,i*1.05,i,i);
-      i*=1.008;
-    } 
-    else{
-      change=true;
-      if (i>200 && !raining){
-        background(i);
-        i-=0.2;
+  if (start){
+    
+    for (let i = 0; i < people.length; i++) {
+      if (people[i].startDeg<people[i].endDeg){ //시계방향
+        people[i].i ++;
       }
+      else{ //반시계방향
+        people[i].i --;
+      }
+      if (people[i].i === people[i].endDeg){
+        people[i].direction = !people[i].direction;
+        people[i].radius = 30+getRandomInt(150)
+        people[i].startDeg = (people[i].endDeg+180)%360;
+        people[i].i = people[i].startDeg;
+        people[i].endDeg =  people[i].direction ? people[i].startDeg+45+getRandomInt(270) :people[i].startDeg-45-getRandomInt(270); 
+        people[i].xAxis = people[i].x-people[i].radius*cos(radians(people[i].i))
+        people[i].yAxis = people[i].y-people[i].radius*sin(radians(people[i].i))
+        
+      }
+      people[i].x=this.xAxis-cos(radians(this.i))*this.radius;
+      people[i].y=this.yAxis-sin(radians(this.i))*this.radius; 
+      people[i].render();
+    }
+    if (mp){
+     
+      if(i<255 && !change){
+        background(i*1.1,i*1.05,i,i);
+        i*=1.008;
+      } 
       else{
-        background(200)
-        raining= true;
-        people = [];
-        for(let i=0; i<180; i++){
-          rain[i].show();
-          rain[i].update();
-          }
-      }
-    }  
-  }
+        change=true;
+        if (i>200 && !raining){
+          background(i);
+          i-=0.2;
+        }
+        else{
+          playS3 = true;
+          if (playS3) sound3.play();
+          playS3 = false;
+          background(200)
+          raining= true;
+          people = [];
+          for(let i=0; i<180; i++){
+            rain[i].show();
+            rain[i].update();
+            }
+        }
+      }  
+    }
+  } 
 }
 
 function mousePressed(){
-  if (!mp){
-    peopleNum = 50;
-    for (let i = 0 ; i < peopleNum-2; i++){
-      people.push(
-      new Person(
-        direction=getRandomInt(1)===0?true:false,
-        startDeg=getRandomInt(360),
-        deg = getRandomInt(90),
-        diameter=10+getRandomInt(20),
-        radius=30+getRandomInt(100),
-        xAxis=windowWidth*Math.random(),
-        yAxis=windowHeight*Math.random(),
-        c=tg[getRandomInt(3)]));
+  if (!start){
+    start = true;
+    sound1.play();
+  }
+  else{
+    if (!mp && start){
+      sound2.play();
+      peopleNum = 200;
+      for (let i = 0 ; i < peopleNum-2; i++){
+        people.push(
+        new Person(
+          direction=getRandomInt(1)===0?true:false,
+          startDeg=getRandomInt(360),
+          deg = getRandomInt(90),
+          diameter=10+getRandomInt(20),
+          radius=30+getRandomInt(100),
+          xAxis=windowWidth*Math.random(),
+          yAxis=windowHeight*Math.random(),
+          c=tg[getRandomInt(3)]));
+      }
+      mp =true;
     }
-    mp =true;
   }
 }
 
